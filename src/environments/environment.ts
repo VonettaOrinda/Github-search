3892e60775b68b5bfd8991f7ddb43a67a572fd3b
VONETTA <thevonetteway@gmail.com>
2022-04-17 22:35:42 +0300

    add personal access token to the environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,6 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+// 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
