ea0430caafc24fee2c2fcca9bd3e086cd559a532
VONETTA <thevonetteway@gmail.com>
2022-04-18 03:02:10 +0300

    add constructors,strings,arrays

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,} from'@angular/common/http';
+import  'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  private username:string;
+  private clientid: 'f49e0f06935c40d04bfc' = "f49e0f06935c40d04bfc";
+  private clientsecret: 'ad7c0aaf0d186710fefb889fed99fb6a9b5abc8d' = "ad7c0aaf0d186710fefb889fed99fb6a9b5abc8d"; 
+  HttpClient: any;
+  
+  
+  constructor(private http:HttpClient) {
+    console.log('service is now ready!');
+    this.username='Vonettaorinda';
+    
+
+   }
+   getProfileInfo(){
+     return new this.HttpClient('https://api.github.com/users/' + this.username + '?client_id=' + 'this.clientid +' ,'&client_secret='+ 'this.clientsecret');
+  //    .map((res: any) => res));
+   }
+}
