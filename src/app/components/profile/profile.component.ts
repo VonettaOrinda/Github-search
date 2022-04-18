4f16b68d120b5f2ce74ce3c42b01afd59eca73b9
VONETTA <thevonetteway@gmail.com>
2022-04-18 14:44:18 +0300

    add profile services

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile: any;
 
   constructor(private ProfileService:ProfileService) { 
     this.ProfileService.getProfileInfo().subscribe((profile: any) =>{ 
       console.log(profile);
+      this.profile=profile;
     });
   }
 
