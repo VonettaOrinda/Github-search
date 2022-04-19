5e5e7f40d4b939c6be84594fad665d783a6058e8
VONETTA <thevonetteway@gmail.com>
2022-04-19 20:35:10 +0300

    add changes made to angular.json, package-lock.json,package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileService } from './services/profile.service';
 import { HttpClientModule} from '@angular/common/http';
-
+// import {AlertModules} from 'ngx-bootstrap'
 
 @NgModule({ 
   declarations: [
     AppComponent,
     ProfileComponent,
+    
     
     
 
@@ -19,8 +20,11 @@ import { HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+   
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  // AlertModules: any
+}
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,23 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
 
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-  profile: any;
 
-  constructor(private ProfileService:ProfileService) { 
-    this.ProfileService.getProfileInfo().subscribe((profile: any) =>{ 
-      console.log(profile);
-      this.profile=profile;
-    });
+export class ProfileComponent implements OnInit {
+  profile!: any;
+  repos!: any[];
+  username!: string;
+  findProfileprofileService: any;
+
+  constructor(private profileService: ProfileService) { 
+
   }
 
-  ngOnInit(): void {
+  findProfile(){
+  	
+  	this.profileService.getProfileInfo().subscribe((profile : any)  => {
+  		console.log(profile);
+  		this.profile = profile;
+  	});
+
+  	this.findProfileprofileService.getProfileRepos().subscribe((repos: any) => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})  	
+  }
+
+  ngOnInit() {
   }
 
 }
+
+
+//   export class ProfileComponent implements OnInit {
+//   [x: string]: any;
+//     profile!: any;
+//     repos!: any;
+//     username!: string;
+
+//   constructor(private ProfileService:ProfileService) { 
+    // this.ProfileService.getProfileInfo().subscribe((profile: any) =>{ 
+    //   console.log(profile);
+    //   this.profile=profile;
+//     });
+//     this['profileService'].getProfileRepos().subscribe((repos: any) => {
+//       console.log(repos);
+//       this.repos=repos;
+//   }
+  
+// }
+
+//  ngOnInit(): void {
+  
+// }
